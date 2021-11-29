138c512507fc4f71358f01dd58c2ca7fe4993511
OhHuyen2002 <thihuyen.maicogroup@gmail.com>
2021-11-29 17:21:30 +0700

    change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def isPrime(x):
         return True
     if (x<=1):
         return False
-    for i in range(2, x-1):
+    for i in range(2, x):
         if(x%i==0):
             return False
     return True
